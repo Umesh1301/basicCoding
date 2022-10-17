d3fd93ad25ccc9e1d2fc01860f52a717a713b55f
umesh1301 <umeshyadav1301@gmail.com>
2022-10-17 00:06:09 -0700

    updated again1

--- insertion sort 1.js
+++ insertion sort 1.js
@@ -1,14 +1,22 @@
-var t,j;
-var a=[4,5,8,7,1,0,-81];
-for(i=1;i<=a.length-1;i++)
+// var t,j;
+// var a=[4,5,8,7,1,0,-81];
+// for(i=1;i<=a.length-1;i++)
+// {
+//     t=a[i];
+//     j=i;
+//     while(j>0 && a[j-1]<=t)
+//     {
+//         a[j]=a[j-1];
+//         --j;
+//     }
+//     a[j]=t;
+// }
+// console.log(a);
+var a=[3,5,9,10,15,0];
+max=a[0];
+for(i=0;i<=5;i++)
 {
-    t=a[i];
-    j=i;
-    while(j>0 && a[j-1]<=t)
-    {
-        a[j]=a[j-1];
-        --j;
-    }
-    a[j]=t;
+    if(max<=a[i])
+    max=a[i];
 }
-console.log(a);
+console.log(max);
